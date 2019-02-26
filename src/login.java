6d40a04ef0d226ccd76f3544f6cba4d6bbac5306
gopi <gopi.ramapuram@gmail.com>
2019-02-26 01:27:39 -0800

    added login.java

---
+++ src/login.java
@@ -0,0 +1,18 @@
+public class Login{
+public static void main (String args[])
+{
+
+String username;
+String pwd;
+ Scanner sc =new Scannner(System.in);
+String userName=sc.next();
+Scanner sc1=new Scanner(System.in);
+String pwd=sc1.next();
+
+System.out.println("userName:"+userName);
+System.out.println("pwd:"+pwd);
+
+
+
+}
+}
